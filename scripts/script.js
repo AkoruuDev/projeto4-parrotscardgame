5b3e45f761d63726ee398600a37bd3d910f8b517
Akoruu.Dev <akoruu.dev@gmail.com>
2022-06-15 15:40:01 -0300

    FEAT [Cartas na Tela]: Adesão da funcionalidade de perguntar quantidade de cartas e mostrar na tela essa quantidade

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+/*
 function escolhaDaQtdDeCartas() {
     let qtdCartas = prompt("Com quantas cartas quer jogar? (de 4 à 14)");
 
@@ -9,3 +10,32 @@ function escolhaDaQtdDeCartas() {
 }
 
 escolhaDaQtdDeCartas()
+*/
+
+
+let qtdCartas = Number(prompt("Com quantas cartas quer jogar?"));
+let contador = 0;
+let cartas = [];
+
+while (contador < qtdCartas) {
+    cartas.push(`
+            <div class="card" data-identifier="card">
+                <div class="frente" data-identifier="front-face">
+                    <img src="/images/front.png" class="parrot">
+                </div>
+                <div class="tras" data-identifier="back-face">
+
+                </div>
+            </div>
+    `)
+
+    colocarNaTela()
+
+    contador = contador + 1;
+}
+
+
+function colocarNaTela() {
+    const parrot = document.querySelector(".container");
+    parrot.innerHTML = cartas;
+}
