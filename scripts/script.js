f515b7eb468785be788161606bdd05efc3dab3d2
Akoruu.Dev <akoruu.dev@gmail.com>
2022-06-20 10:18:17 -0300

    FEAT[Conferir Cartas]: Finalizado a implementação de conferencia de cartas para cada par selecionado

--- scripts/script.js
+++ scripts/script.js
@@ -3,66 +3,88 @@ let qtdCartas;
 while (stop != 'Stop') {
     qtdCartas = prompt("Com quantas cartas quer jogar? (pares de 4 à 14)");
 
-    if (qtdCartas != 4 && qtdCartas != 6 && qtdCartas != 8 && qtdCartas != 10 && qtdCartas != 12 && qtdCartas != 14) {
-        stop = 'continua';
-    } else {
+    if (qtdCartas % 2 == 0 && qtdCartas < 15 && qtdCartas > 3)  {
         stop = 'Stop';
     }
 }
 
 const todasAsCartas = [
-    {nome: 'bobrossparrot.gif', numero: 'one'},
-    {nome: 'explodyparrot.gif', numero: 'two'},
-    {nome: 'fiestaparrot.gif', numero: 'Three'},
-    {nome: 'metalparrot.gif', numero: 'four'},
-    {nome: 'revertitparrot.gif', numero: 'five'},
-    {nome: 'tripletsparrot.gif', numero: 'six'},
-    {nome: 'unicornparrot.gif', numero: 'seven'}
+    'bobrossparrot.gif',
+    'explodyparrot.gif',
+    'fiestaparrot.gif',
+    'metalparrot.gif',
+    'revertitparrot.gif',
+    'tripletsparrot.gif',
+    'unicornparrot.gif'
 ]
 
 let backCards = [];
 
-for (let index = 0; index < (qtdCartas/2); index++) {
-    backCards.push(todasAsCartas[index]);
-}
+function lancarCartas () {
+    for (let index = 0; index < (qtdCartas/2); index++) {
+        backCards.push(todasAsCartas[index]);
+        backCards.push(todasAsCartas[index]);
+    }
 
-for (let index = 0; index < (qtdCartas/2); index++) {
-    backCards.push(backCards[index]);
-}
+    backCards.sort(comparador);
 
-backCards.sort(comparador);
+    function comparador() { 
+        return Math.random() - 0.5; 
+    }
 
-function comparador() { 
-	return Math.random() - 0.5; 
-}
-
-for (let index = 0; index < qtdCartas; index++) {
-    const cartas = `
-        <div class="card ${backCards[index].numero}" onclick="flip(this)" data-identifier="card">
-            <div class="face front" data-identifier="front-face">
-                <img src="./images/front.png" class="parrot">
+    for (let index = 0; index < qtdCartas; index++) {
+        const cartas = `
+            <div class="card" onclick="flip(this)" data-identifier="card">
+                <div class="face front" data-identifier="front-face">
+                    <img src="./images/front.png" class="parrot">
+                </div>
+                <div class="face back" data-identifier="back-face">
+                    <img src="./images/${backCards[index]}" class="img">
+                </div>
             </div>
-            <div class="face back" data-identifier="back-face">
-                <img src="./images/${backCards[index].nome}" class="img">
-            </div>
-        </div>
-    `;
-    document.querySelector(".container").innerHTML += cartas;
+        `;
+        document.querySelector(".container").innerHTML += cartas;
+    }
 }
 
-let cardVirado = '';
+lancarCartas();
+
 let tentativas = 0;
 
 function flip(elemento) {
+    tentativas ++;
     elemento.classList.add("flip");
-    verificarCartas ();
 
-    contarTentativas();
-    conferirJogo();
+    let verificador = document.querySelectorAll(".virado");
+    if (verificador.length <= 1) {
+        elemento.classList.add("virado");
+        testeDeComparacao();
+    }
 }
 
-function contarTentativas() {
-    tentativas = tentativas + 1;
+function testeDeComparacao() {
+    let verificador = document.querySelectorAll(".virado");
+    if (verificador.length == 2) {
+        setTimeout (comparar,1200)
+    }
+}
+
+function comparar(){
+    let verificador = document.querySelectorAll(".virado");
+    if (verificador.length == 2) {
+        let carta1 = verificador[0].querySelector(".img").currentSrc;
+        let carta2 = verificador[1].querySelector(".img").currentSrc;
+        if (carta1 == carta2) {
+            verificador[0].classList.remove("virado");
+            verificador[1].classList.remove("virado");
+        } else {
+            verificador[0].classList.remove("virado");
+            verificador[1].classList.remove("virado");
+            verificador[0].classList.remove("flip");
+            verificador[1].classList.remove("flip");
+        }
+    }
+    conferirJogo (); 
 }
 
 function conferirJogo() {
@@ -70,17 +92,4 @@ function conferirJogo() {
     if (cartasNaMesa == qtdCartas) {
         alert(`Parabéns! Você finalizou o jogo com ${tentativas} tentativas!`);
     }
-}
-
-function verificarCartas () {
-    let novoCard = document.querySelector("elemento .img");
-        if (cardVirado == '') {
-            if (cardVirado != novoCard) {
-                cardVirado.classList.remove("flip");
-                novoCard.classList.remove("flip");
-                cardVirado = '';
-            }
-        } else {
-            cardVirado = document.querySelector("elemento .img");
-        }
 }
